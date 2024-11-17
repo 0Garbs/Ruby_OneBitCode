b8168a48de9dc315aa1221fc24bb0755f721dd13
Gabriel.Silva <gabriel.silva@areco.net.br>
2024-11-17 09:13:20 -0300

    FT: Seeing how use inputs in Ruby

---
+++ aula2/input.rb
@@ -0,0 +1,9 @@
+# Saída de dados
+print 'Digite seu nome: '
+
+# Entrada de dados
+name = gets.chomp
+
+# Saída utilizando o puts
+# Utilize códigos ruby dentro de uma string com #{code}
+puts "Hello #{name}!"
