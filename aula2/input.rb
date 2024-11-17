0eef161ffe06848ac0775b7401ee1dc079f2f90a
Gabriel.Silva <gabriel.silva@areco.net.br>
2024-11-17 09:20:36 -0300

    FT: Adding a input for last name

--- aula2/input.rb
+++ aula2/input.rb
@@ -4,6 +4,9 @@ print 'Digite seu nome: '
 # Entrada de dados
 name = gets.chomp
 
+print 'Digite seu sobrenome: '
+lastName = gets.chomp
+
 # Saída utilizando o puts
 # Utilize códigos ruby dentro de uma string com #{code}
-puts "Hello #{name}!"
+puts "Hello #{name} #{lastName}!"
